f1e78fc0a04a146290c2fa52acb5c544b02402be
Auriol Benjamin <auriolbenjamin@MacBook-Air.local>
2025-11-04 18:51:01 +0100

    fix: add Redis config and workerMode to medusa-config

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,12 +5,14 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors: process.env.STORE_CORS || "http://localhost:8000,https://docs.medusajs.com",
+      adminCors: process.env.ADMIN_CORS || "http://localhost:5173,http://localhost:9000,https://docs.medusajs.com",
+      authCors: process.env.AUTH_CORS || "http://localhost:5173,http://localhost:9000,https://docs.medusajs.com",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
+    workerMode: process.env.MEDUSA_WORKER_MODE === "worker" ? "worker" : "server"
   }
 })
