55dbad4a0047f714d2644c039d61a1fd30ea12e5
Auriol Benjamin <auriolbenjamin@MacBook-Air.local>
2025-11-04 18:36:59 +0100

    fix: optimize Railway build - remove problematic scripts and add railway config

---
+++ .medusa/server/public/admin/assets/chunk-F6ZOHZVB-BR4ufINB.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-C52cQz5B.js";var e=({children:s})=>r.jsx("span",{className:"sr-only",children:s});export{e as V};
---
+++ .medusa/server/public/admin/assets/chunk-TDCEJDCA-g8GbFMTd.js
@@ -0,0 +1,1 @@
+import{aP as i}from"./index-C52cQz5B.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
